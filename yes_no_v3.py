7eb7ae2e97a37d250208dde27610f780e58988cf
twschool <98859425+twschool@users.noreply.github.com>
2022-03-28 12:02:43 +1300

    Coded more on yes_no_v3
    changed the return statement to be a list to display [True or False if invalid answer, "] My code is so complicated that I feel sorry for not following the teachers example. If you enter being a new player it asks you if you are having fun. I also moved the input question statement out of the function so that the yes_no function could be used for anything. This resulted in more code in the main routine but a function that can be used for anything and not just once.

--- yes_no_v3.py
+++ yes_no_v3.py
@@ -6,23 +6,39 @@ y or n as abbreviations. Prints result of user choice as well as input - For tes
 
 # Function that asks the user if they have played the game before
 # The function loops until an answer of yes or no has been given
-def show_instructions():
-    looping_ = True
-    while looping_:
-        response = input("Have you played this game before? : ").upper()
-        if response == "Y" or response == "YES":
-            # Temporary text to make it easier to test and check inputs
-            print("Program continues")
-            print(f"You entered {response}")
-            return
-        elif response == "N" or response == "NO":
-            print("Display instructions")
-            print(f"You entered {response}")
-            return
-        else:
-            print('Please enter either “Yes” or “No”')
-            print(f"You entered {response}")
+def yes_no_func(response):
+    # If they say yes then skip and output "Program continues"
+    if response == "Y" or response == "YES":
+        print("Program continues")
+        print(f"You entered {response.title()}")
+        # Returns False so the loop breaks as a non-invalid answer was entered
+        return [False, "Yes"]
+    # If they say no, output "Display instructions"
+    elif response == "N" or response == "NO":
+        print("Display instructions")
+        print(f"You entered {response.title()}")
+        return [False, "No"]
+    else:
+        print('Please enter either “Yes” or “No”')
+        print(f"You entered {response.title()}")
+        # Returns True as a Valid answer was entered
+        return [True, ""]
 
 
 # Main routine
-show_instructions()
+looping = [True, ""]
+response_ = ""
+while looping[0] is not False:
+    response_ = input("Have you played this game before? : ").upper()
+    looping = yes_no_func(response_)
+if looping[1] == "No":
+    looping[0] = True
+    while looping[0] is not False:
+        response_ = input("Are you having fun? : ").upper()
+        looping = yes_no_func(response_)
+    if looping[1] == "No":
+        print("That's bad")
+    else:
+        print("Good")
+elif looping[1] == "Yes":
+    print("We have a regular")
