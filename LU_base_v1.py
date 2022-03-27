cff3980217acd8531acc77508f23c36d754d096e
twschool <98859425+twschool@users.noreply.github.com>
2022-03-28 12:53:22 +1300

    Create LU_base_v1.py
    Added base v1 with no changes from instructions code apart from a comment

---
+++ LU_base_v1.py
@@ -0,0 +1,45 @@
+"""LU base component
+Components added after they have been created and tested"""
+
+
+# The function loops until an answer of yes or no has been given
+# Yes/no checking function
+def yes_no_func(response):
+    # If they say yes then skip and output "Program continues"
+    if response == "Y" or response == "YES":
+        print("Program continues")
+        print(f"You entered {response.title()}")
+        # Returns False so the loop breaks as a non-invalid answer was entered
+        return [False, "Yes"]
+    # If they say no, output "Display instructions"
+    elif response == "N" or response == "NO":
+        print("Display instructions")
+        print(f"You entered {response.title()}")
+        return [False, "No"]
+    else:
+        print('Please enter either “Yes” or “No”')
+        print(f"You entered {response.title()}")
+        # Returns True as a Valid answer was entered
+        return [True, ""]
+
+
+# Function to display instructions
+def show_instructions():
+    print("**** How to play ****")
+    print()
+    print("The rules of the game will go here")
+    print()
+    print("Program continues")
+    print()
+
+
+# Main routine
+looping = [True, ""]
+response_ = ""
+played_before = ""
+while looping[0] is not False:
+    response_ = input("Have you played this game before? : ").upper()
+    looping = yes_no_func(response_)
+    played_before = looping[1]
+if played_before == "No":
+    show_instructions()
