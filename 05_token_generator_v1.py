f8e6195391dbbaa5f56919250c0d051c1a0bca6b
twschool <98859425+twschool@users.noreply.github.com>
2022-04-04 11:10:03 +1200

    Added Token Generator V1
    Added the random token generator script. Currently a test script that gives the user 100$ and run the random token generator betting thing 100 times. I rigged the game more torward the house as the house was regularlly losing money

---
+++ 05_token_generator_v1.py
@@ -0,0 +1,22 @@
+"""Component 3 (random token) v1
+Generates random choice of token in random order"""
+import random
+
+
+def token_generator():
+    money = 100
+    for num in range(0, 100):
+        token = ["Horse", "Donkey", "Unicorn", "Zebra", "Zebra", "Donkey", "Horse", "Zebra", "Donkey", "Horse"]
+        token_choice = random.choice(token)
+        if token_choice == "Unicorn":
+            money = money + 4
+        elif token_choice == "Horse" or token_choice == "Donkey":
+            money = money - 0.5
+        else:
+            money = money - 1
+        print(money)
+
+
+
+# Main routine
+token_generator()
