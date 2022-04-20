2060fa75c0b2e94bb63a0723f187e7cfb1b64684
twschool <98859425+twschool@users.noreply.github.com>
2022-04-20 13:33:04 +1200

    Changed final version to fit pep8
    Changed final version to fit pep8 standards

--- LU_final_version.py
+++ LU_final_version.py
@@ -46,7 +46,8 @@ def show_instructions():
     print()
     print("Choose a starting amount to play with - must be between $1 and $10")
     print()
-    print("Then pressed <enter> to play. You will get a random token which might "
+    print("Then pressed <enter> to play. You will get a"
+          "random token which might "
           "be a horse, a\nzebra, a donkey, or a unicorn")
     print()
     print("It costs $1 to play each round but, depending on your prize, you "
@@ -73,46 +74,57 @@ def token_generator(money):
         print(formatter(".", f"Round {round_number}"))
         print()
         # Random list off tokens that the random function chooses from
-        # I wrote my own code for this so that is why it's not a random number from 0-100 choosing
+        # I wrote my own code for this so that is why
+        # it's not a random number from 0-100 choosing
         token = ["Horse", "Horse", "Horse", "Horse", "Horse",
                  "Donkey", "Donkey", "Donkey", "Donkey", "Donkey",
                  "Zebra", "Zebra", "Zebra", "Zebra", "Zebra",
                  "Unicorn"]
-        # token_choice is the variable that the random token choice is stored inside
+        # token_choice is the variable that
+        # the random token choice is stored inside
         token_choice = random.choice(token)
         # if the token is a unicorn then 4$ is added to the users balance
         if token_choice == "Unicorn":
             money = money + 4
-            print(formatter("!", f"Congratulations, you got a unicorn, Your balance is now ${money:.2f}"))
+            print(formatter("!", f"Congratulations, you got a"
+                            f" unicorn, Your balance is now ${money:.2f}"))
             print()
         # if the token is a horse or a donkey 50c is added to the users balance
         elif token_choice == "Horse":
             money -= 0.5
-            print(formatter("H", f"You got a horse, Your balance is now ${money:.2f}"))
+            print(formatter("H", f"You got a horse,"
+                            f" Your balance is now ${money:.2f}"))
             print()
         elif token_choice == "Zebra":
             money -= 0.5
-            print(formatter("Z", f"You got a Zebra, Your balance is now ${money:.2f}"))
+            print(formatter("Z", f"You got a Zebra,"
+                            f" Your balance is now ${money:.2f}"))
             print()
         # if the token is not either above then the token must be a donkey
         # if the token is a zebra 1$ is taken away from the user
         else:
             money = money - 1
-            print(formatter("D", f"Bad luck, you got a donkey, Your balance is now ${money:.2f}"))
+            print(formatter("D", f"Bad luck, you got a donkey,"
+                            f" Your balance is now ${money:.2f}"))
             print()
         if money <= 0:
-            # if the user runs out of money then the function will return the bool value
+            # if the user runs out of money then the
+            # function will return the bool value
             # True as it is True that the user ran out of money
             print(f"Sorry but you have ran out of money\n")
             print(formatter("=", "Thanks for playing"))
-            print(f"\nYou started with ${starting_money:.2f}\nand leave with ${money:.2f}")
+            print(f"\nYou started with ${starting_money:.2f}\n"
+                  f" and leave with ${money:.2f}")
             return True
         quitting = input(f"<enter> to continue or X to exit: ").upper()
         if quitting == "X":
             print(formatter("=", "Thanks for playing"))
-            print(f"You started with ${starting_money:.2f}\nand leave with ${money:.2f}")
-            # if the user decides to optionally quit with the money they currently have
-            # the function will return the value false as it is False that the user ran out of money
+            print(f"You started with ${starting_money:.2f}\n"
+                  f" and leave with ${money:.2f}")
+            # if the user decides to optionally quit
+            # with the money they currently have
+            # the function will return the value false
+            # as it is False that the user ran out of money
             return False
         else:
             print("Continuing")
@@ -135,19 +147,25 @@ response_ = ""
 played_before = ""
 while looping[0] is not False:
     response_ = input("Have you played this game before? : ").upper()
-    # This code runs the answer through a function to determine if the answer is a
+    # This code runs the answer through a function
+    # to determine if the answer is a
     # Valid answer (Either 'yes' or 'no')
     looping = yes_no_func(response_)
     played_before = looping[1]
 if played_before == "No":
-    # If the user has not played before then this code will call the function
+    # If the user has not played before
+    # then this code will call the function
     # That prints the instructions
     show_instructions()
-user_balance_ = number_checker("How much would you like to play with? $", 1, 10)
-# Tells the user how much money they are playing with based on the input they put in
+user_balance_ = number_checker("How much"
+                               " would you like to play with? $", 1, 10)
+# Tells the user how much money they are
+# playing with based on the input they put in
 print(f"You are playing with ${user_balance_}")
-# Force quit is a variable that is set to True when the user runs out of money and set to False
-# when the user decides to optionally quit the game with the amount of money they currently have
+# Force quit is a variable that is set to
+# True when the user runs out of money and set to False
+# when the user decides to optionally quit the game
+# with the amount of money they currently have
 force_quit = token_generator(user_balance_)
 if force_quit is True:
     # Code which ends the program when the user runs out of money
